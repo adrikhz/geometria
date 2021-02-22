2d014f1a738d0ced6f56b0c4809fe807671a14a6
Adrián Vicente Maciá <adrikhz@gmail.com>
2021-02-22 13:47:07 +0100

    versión refactorizada

--- geometria/src/Circulo_AVM.java
+++ geometria/src/Circulo_AVM.java
@@ -1,10 +1,10 @@
 
-public class Circulo extends FiguraGeometrica {
+public class Circulo_AVM extends FiguraGeometrica_AVM {
 	private double radio;
 	//Constante pi
 	static final double PI = 3.1416;
 	
-	public Circulo(double r, String tipoFigura) {
+	public Circulo_AVM(double r, String tipoFigura) {
 		super(tipoFigura);
 		radio = r;
 	}
--- geometria/src/FiguraGeometrica_AVM.java
+++ geometria/src/FiguraGeometrica_AVM.java
@@ -1,9 +1,9 @@
 
-public abstract class FiguraGeometrica {
+public abstract class FiguraGeometrica_AVM {
 
 	protected String tipoFigura;
 
-	public FiguraGeometrica(String tipoFigura) {
+	public FiguraGeometrica_AVM(String tipoFigura) {
 		super();
 		this.tipoFigura = tipoFigura;
 	}
